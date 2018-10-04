c51de6ccf6bb658d3e4a81ccff63105bab5ff5a7
Matthias Otto <matthias.otto.hb@gmail.com>
2018-10-04 13:58:44 -0600

    Adding Test controller to test network delay

---
+++ DynamoTest/Controllers/TestController.cs
@@ -0,0 +1,32 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Amazon.Lambda.Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DynamoTest.Controllers
+{
+    public class TestController : Controller
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            LambdaLogger.Log($"Get Test invoked");
+
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            var stringTask = client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
+
+            var msg = await stringTask;
+
+            LambdaLogger.Log($"Retrieved string from Github");
+
+            return msg;
+        }
+    }
+}
